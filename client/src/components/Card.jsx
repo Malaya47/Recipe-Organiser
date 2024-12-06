6cd950173c1c3a7cdf9c28462b3ca27cdee5434b
Malaya Tiwari <sidzilegend@gmail.com>
2024-12-06 18:06:04 +0530

    delete api fix

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const Card = ({ recipies, getAllRecipies }) => {
   const deleteRecipeHandler = async (id) => {
     const response = await axios.delete(
-      `https://recipe-organiser-backend-three.vercel.app/${id}`
+      `https://recipe-organiser-backend-three.vercel.app/deleteRecipe/${id}`
     );
 
     getAllRecipies();
