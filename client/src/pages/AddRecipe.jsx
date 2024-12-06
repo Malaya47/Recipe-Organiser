437ad515ee482357d49f9efab9314f7fc955be8e
Malaya Tiwari <sidzilegend@gmail.com>
2024-12-06 14:38:42 +0530

    update: add recipe form

--- client/src/pages/AddRecipe.jsx
+++ client/src/pages/AddRecipe.jsx
@@ -1,46 +1,118 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
 
 const AddRecipe = () => {
+  const [recipeName, setRecipeName] = useState("");
+  const [cuisineType, setCuisineType] = useState("");
+  const [imageLink, setImageLink] = useState("");
+  const [ingredients, setIngredients] = useState("");
+  const [instructions, setInstructions] = useState("");
+
+  const recipeSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    const recipeData = {
+      recipeName,
+      cuisineType,
+      imageLink,
+      ingredients: ingredients.split("\n"),
+      instructions: instructions.split("\n"),
+    };
+
+
+    const response = await axios.post(
+      "http://localhost:3000/recipe/addRecipe",
+      recipeData
+    );
+
+   
+
+    setRecipeName("");
+    setCuisineType("");
+    setImageLink("");
+    setIngredients("");
+    setInstructions("");
+  };
+
   return (
     <section>
       <Header />
 
       <section className="container mt-5">
         <h2 className="mb-4">Add Recipe</h2>
-        <form style={{ width: "20rem" }}>
+        <form onSubmit={recipeSubmitHandler} style={{ width: "20rem" }}>
           <div className="mb-3">
-            <label className="form-label" htmlFor="">
+            <label className="form-label" htmlFor="recipeName">
               Name:
             </label>
-            <input className="form-control" type="text" />
+            <input
+              id="recipeName"
+              className="form-control"
+              type="text"
+              value={recipeName}
+              onChange={(e) => setRecipeName(e.target.value)}
+              required
+            />
           </div>
 
           <div className="mb-3">
-            <label className="form-label" htmlFor="">
+            <label className="form-label" htmlFor="cuisineType">
               Cuisine Type:
             </label>
-            <input className="form-control" type="text" />
+            <input
+              id="cuisineType"
+              className="form-control"
+              type="text"
+              value={cuisineType}
+              onChange={(e) => setCuisineType(e.target.value)}
+              required
+            />
           </div>
+
           <div className="mb-3">
-            <label className="form-label" htmlFor="">
+            <label className="form-label" htmlFor="imageLink">
               Image Link:
             </label>
-            <input className="form-control" type="text" />
+            <input
+              id="imageLink"
+              className="form-control"
+              type="text"
+              value={imageLink}
+              onChange={(e) => setImageLink(e.target.value)}
+              required
+            />
           </div>
+
           <div className="mb-3">
-            <label className="form-label" htmlFor="">
+            <label className="form-label" htmlFor="ingredients">
               Ingredients:
             </label>
-            <textarea className="form-control"></textarea>
+            <textarea
+              id="ingredients"
+              className="form-control"
+              value={ingredients}
+              onChange={(e) => setIngredients(e.target.value)}
+              rows="5"
+              required
+            ></textarea>
           </div>
+
           <div className="mb-3">
-            <label className="form-label" htmlFor="">
+            <label className="form-label" htmlFor="instructions">
               Instructions:
             </label>
-            <textarea className="form-control"></textarea>
+            <textarea
+              id="instructions"
+              className="form-control"
+              value={instructions}
+              onChange={(e) => setInstructions(e.target.value)}
+              rows="5"
+              required
+            ></textarea>
           </div>
-          <button className="btn btn-primary" type="submit">
+
+          <button className="btn btn-primary mb-3" type="submit">
             Submit
           </button>
         </form>
